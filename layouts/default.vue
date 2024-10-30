ead3ff1869b04d75b06b8e04c2a78f7320911807
codeGerald131313 <geraldsandy@outlook.es>
2024-10-30 00:04:45 -0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,42 @@
 <template>
-
-    <body class="header-sticky header-over">
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8K2TJG6" height="0" width="0"
-                style="display:none;visibility:hidden"></iframe>
-        </noscript>
-        <div id="wrapper">
-            <Header />
-            <nuxt />
-            <Footer />
-        </div>
-    </body>
-</template>
-
-<script>
-
-import Footer from '../components/Footer.vue';
-import Header from '../components/Header.vue';
-
-export default {
+    <div>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8K2TJG6" 
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      </noscript>
+      <!-- End Google Tag Manager (noscript) -->
+      
+      <div id="wrapper">
+        <Header />
+        <nuxt />
+        <Footer />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Footer from '../components/Footer.vue';
+  import Header from '../components/Header.vue';
+  
+  export default {
     name: 'default',
     components: {
-        Header,
-        Footer,
+      Header,
+      Footer,
+    },
+    mounted() {
+      // Agrega la clase al body cuando el componente está montado
+      document.body.classList.add('header-sticky', 'header-over');
+    },
+    beforeDestroy() {
+      // Elimina la clase del body cuando el componente se destruye
+      document.body.classList.remove('header-sticky', 'header-over');
     }
-}
-</script>
-
-<style>
-/* Estilos globales para el diseño */
-</style>
+  }
+  </script>
+  
+  <style>
+  /* Estilos globales para el diseño */
+  </style>
+  
