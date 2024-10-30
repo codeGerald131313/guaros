12a66983fbdceb6f83e293d6b3cafacd16bd86f4
codeGerald131313 <geraldsandy@outlook.es>
2024-10-30 00:19:45 -0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,27 +14,43 @@
       </div>
     </div>
   </template>
-  
-  <script>
-  import Footer from '../components/Footer.vue';
-  import Header from '../components/Header.vue';
-  
-  export default {
-    name: 'default',
-    components: {
-      Header,
-      Footer,
-    },
-    mounted() {
-      // Agrega la clase al body cuando el componente está montado
-      document.body.classList.add('header-sticky', 'header-over');
-    },
-    beforeDestroy() {
+
+
+<script>
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+
+export default {
+  name: 'default',
+  components: {
+    Header,
+    Footer,
+  },
+  mounted() {
+
+     document.body.classList.add('header-sticky', 'header-over');
+    // Inyecta el Google Tag Manager en el <head>
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N8K2TJG6');
+    `;
+    document.head.appendChild(script);
+  }
+     beforeDestroy() {
       // Elimina la clase del body cuando el componente se destruye
       document.body.classList.remove('header-sticky', 'header-over');
     }
-  }
-  </script>
+}
+</script>
+
+
+
   
   <style>
   /* Estilos globales para el diseño */
