4c32f860f8d83a57269d2d035a8738c3a9f3c54b
codeGerald131313 <geraldsandy@outlook.es>
2024-10-30 00:13:42 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,20 +26,20 @@ export default {
         async: true
       },
       {
-        innerHTML: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-SSMQYY2R5Q');
-        `,
-        type: 'text/javascript',
+        json: {
+          dataLayer: [],
+          config: {
+            id: 'G-SSMQYY2R5Q'
+          }
+        },
+        type: 'application/json',
         charset: 'utf-8'
       },
       // Google Tag Manager
       {
         innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-N8K2TJG6');`,
         type: 'text/javascript',
