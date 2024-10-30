5f2a27e982a16716d14276b8b0944e0a387420b7
codeGerald131313 <geraldsandy@outlook.es>
2024-10-30 16:10:38 -0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,28 @@
 <template>
-    <div>
 
-      <!-- End Google Tag Manager (noscript) -->
-      
-      <div id="wrapper">
-        <Header />
-        <nuxt />
-        <Footer />
-      </div>
-    </div>
-  </template>
-
+    <body class="header-sticky header-over">
+        <div id="wrapper">
+            <Header />
+            <nuxt />
+            <Footer />
+        </div>
+    </body>
+</template>
 
 <script>
+
 import Footer from '../components/Footer.vue';
 import Header from '../components/Header.vue';
 
 export default {
-  name: 'default',
-  components: {
-    Header,
-    Footer,
-  },
-  mounted() {
-    // Agrega la clase al body cuando el componente está montado
-    document.body.classList.add('header-sticky', 'header-over');
-    
-  },
-  beforeDestroy() {
-    // Elimina la clase del body cuando el componente se destruye
-    document.body.classList.remove('header-sticky', 'header-over');
-  }
+    name: 'default',
+    components: {
+        Header,
+        Footer,
+    }
 }
-
 </script>
 
-
-
-  
-  <style>
-  /* Estilos globales para el diseño */
-  </style>
-  
+<style >
+/* Estilos globales para el diseño */
+</style>
